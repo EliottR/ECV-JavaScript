c9f550b0c68a32a38a273028cb1f51612c653504
Eliott Raheriarisoa <eliott.raheriarisoa@gmail.com>
2022-04-06 13:45:21 +0200

    [tp5] exo5 + exo4

--- TP5/script.js
+++ TP5/script.js
@@ -7,8 +7,6 @@ const getHashTags = (str) => {
     return result
 }
 
-console.log(getHashTags("How the Avocado Became the Fruit of the Global Trade"));
-
 //2
 const removeDuplicate = (arr) => {
     arr.map((value, index) => { arr[index] == arr[index - 1] ? arr.splice(index, 1) : '' })
@@ -17,20 +15,26 @@ const removeDuplicate = (arr) => {
 
 const tab = [0, 2, 4, 6, 8, 8]
 
-console.log(removeDuplicate(tab))
-
 //3
 const intersection = (arr, arr2) => {
     const finalArr = arr.filter(element => arr2.includes(element));
     return finalArr
 }
 
-console.log(intersection(first, second))
-
 //4
 const arrayDiff = (arr, arr2) => {
-    const finalArr = arr.filter(element => !arr2.includes(element))
+    const finalArr = removeDuplicate([...arr.filter(element => !arr2.includes(element)), ...arr2.filter(element => !arr.includes(element))])
     return finalArr
 }
 
+//5
+const combinations = (...elements) => {
+    const comb = elements.reduce((prev, current) => prev * current)
+    return comb
+}
+
+console.log(getHashTags("How the Avocado Became the Fruit of the Global Trade"));
+console.log(removeDuplicate(tab))
+console.log(intersection(first, second))
 console.log(arrayDiff(first, second))
+console.log(combinations(2, 3, 4, 5))
